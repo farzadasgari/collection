589915b95edbe90d04dec945fb9808ddec52357b
Farzad Asgari <khufarzadasgari@gmail.com>
2024-08-23 22:28:58 +0330

    add chatbox scripts

--- assets/js/collection.js
+++ assets/js/collection.js
@@ -2,3 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     var navbarHeight = document.querySelector('.navbar').offsetHeight;
 document.body.style.paddingTop = navbarHeight + 'px';
 });
+
+document.getElementById('chatButton').addEventListener('click', function() {
+    var chatBox = document.getElementById('chatBox');
+    chatBox.classList.toggle('show');
+});
